7d462e23b31a7aca0953cffa1aebc24b46273bb8
Jon Schneider <jschneider2013@gmail.com>
2017-04-22 01:01:35 -0400

    added some scaling tweens

--- js/game.js
+++ js/game.js
@@ -1,45 +1,69 @@
 var PhaserGame = (function () {
     function PhaserGame() {
+        var _this = this;
+        this.scaleToPlayer = function () {
+            for (var _i = 0, _a = _this.objects.children; _i < _a.length; _i++) {
+                var object = _a[_i];
+                var tween = _this.game.add.tween(object.scale).to({ x: 0.5, y: 0.5 }, 1000);
+                tween.start();
+                var midpoint = new PIXI.Point(0.5 * (object.position.x + _this.player.position.x), 0.5 * (object.position.y + _this.player.position.y));
+                tween = _this.game.add.tween(object).to(midpoint);
+                tween.start();
+            }
+        };
+        this.preload = function () {
+            _this.game.time.advancedTiming = true;
+        };
+        this.create = function () {
+            _this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            _this.cursors = _this.game.input.keyboard.createCursorKeys();
+            var playerBMD = _this.game.add.bitmapData(32, 32);
+            playerBMD.ctx.beginPath();
+            playerBMD.ctx.rect(0, 0, 32, 32);
+            playerBMD.ctx.fillStyle = '#ff0000';
+            playerBMD.ctx.fill();
+            _this.player = _this.game.add.sprite(32, 32, playerBMD);
+            _this.game.physics.arcade.enable(_this.player);
+            var objectBMD = _this.game.add.bitmapData(32, 32);
+            objectBMD.ctx.beginPath();
+            objectBMD.ctx.rect(0, 0, 32, 32);
+            objectBMD.ctx.fillStyle = '#0000ff';
+            objectBMD.ctx.fill();
+            _this.objects = _this.game.add.group();
+            _this.objects.enableBody = true;
+            for (var i = 0; i < 10; i++) {
+                var object = _this.objects.create(Math.random() * 600, Math.random() * 600, objectBMD);
+                object.body.immovable = true;
+            }
+        };
+        this.update = function () {
+            _this.game.debug.text(String(_this.game.time.fps), 2, 14, "#00ff00");
+            _this.game.physics.arcade.collide(_this.player, _this.objects);
+            if (_this.cursors.left.isDown) {
+                _this.player.body.velocity.x = -150;
+            }
+            else if (_this.cursors.right.isDown) {
+                _this.player.body.velocity.x = 150;
+            }
+            else {
+                _this.player.body.velocity.x = 0;
+            }
+            if (_this.cursors.up.isDown) {
+                _this.player.body.velocity.y = -150;
+            }
+            else if (_this.cursors.down.isDown) {
+                _this.player.body.velocity.y = 150;
+                _this.scaleToPlayer();
+            }
+            else {
+                _this.player.body.velocity.y = 0;
+            }
+        };
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload,
             create: this.create,
             update: this.update
         });
     }
-    PhaserGame.prototype.preload = function () {
-        this.game.time.advancedTiming = true;
-    };
-    PhaserGame.prototype.create = function () {
-        var bmd = this.game.add.bitmapData(32, 32);
-        bmd.ctx.beginPath();
-        bmd.ctx.rect(0, 0, 32, 32);
-        bmd.ctx.fillStyle = '#ff0000';
-        bmd.ctx.fill();
-        this.player = this.game.add.sprite(32, 32, bmd);
-        this.game.physics.arcade.enable(this.player);
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-    };
-    PhaserGame.prototype.update = function () {
-        this.game.debug.text(String(this.game.time.fps), 2, 14, "#00ff00");
-        if (this.cursors.left.isDown) {
-            this.player.body.velocity.x = -150;
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.body.velocity.x = 150;
-        }
-        else {
-            this.player.body.velocity.x = 0;
-        }
-        if (this.cursors.up.isDown) {
-            this.player.body.velocity.y = -150;
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.body.velocity.y = 150;
-        }
-        else {
-            this.player.body.velocity.y = 0;
-        }
-    };
     return PhaserGame;
 }());
 window.onload = function () {
--- src/game.ts
+++ src/game.ts
@@ -5,6 +5,7 @@ class PhaserGame {
     game: Phaser.Game;
     player: Phaser.Sprite;
     cursors: Phaser.CursorKeys;
+    objects: Phaser.Group;
 
     constructor() {
         this.game = new Phaser.Game(800, 
@@ -18,49 +19,70 @@ class PhaserGame {
 
     } 
 
-    preload() {
+    scaleToPlayer = () => {
+        for(let object of this.objects.children){
+            
+            let tween = this.game.add.tween(object.scale).to({x: 0.5, y: 0.5}, 1000);
+            tween.start();
+
+            let midpoint = new PIXI.Point(0.5*(object.position.x+this.player.position.x),0.5*(object.position.y+this.player.position.y));
+            tween = this.game.add.tween(object).to(midpoint);
+            tween.start();
+        }
+    }
+
+    preload = () => {
         this.game.time.advancedTiming = true;
     }
 
-    create() {
-        let bmd = this.game.add.bitmapData(32,32);
-
-        bmd.ctx.beginPath();
-        bmd.ctx.rect(0,0,32,32);
-        bmd.ctx.fillStyle = '#ff0000';
-        bmd.ctx.fill();
-
-        // use the bitmap data as the texture for the sprite
-        this.player = this.game.add.sprite(32, 32, bmd);
-        this.game.physics.arcade.enable(this.player);
-
+    create = () => {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        let playerBMD = this.game.add.bitmapData(32,32);
+
+        playerBMD.ctx.beginPath();
+        playerBMD.ctx.rect(0,0,32,32);
+        playerBMD.ctx.fillStyle = '#ff0000';
+        playerBMD.ctx.fill();
+
+        this.player = this.game.add.sprite(32, 32, playerBMD);
+        this.game.physics.arcade.enable(this.player);
+
+        let objectBMD = this.game.add.bitmapData(32, 32);
+        objectBMD.ctx.beginPath();
+        objectBMD.ctx.rect(0,0,32,32);
+        objectBMD.ctx.fillStyle = '#0000ff';
+        objectBMD.ctx.fill();
+
+        this.objects = this.game.add.group();
+        this.objects.enableBody = true;
+        for(let i=0;i<10;i++){
+            let object: Phaser.Sprite = this.objects.create(Math.random()*600, Math.random()*600, objectBMD);
+            object.body.immovable = true;
+        }
+
     }
 
-    update() {
+    update = () =>  {
         this.game.debug.text(String(this.game.time.fps), 2, 14, "#00ff00");
 
-        if (this.cursors.left.isDown)
-        {
+        this.game.physics.arcade.collide(this.player, this.objects);
+
+        if (this.cursors.left.isDown){
             this.player.body.velocity.x = -150;
-        }
-        else if (this.cursors.right.isDown)
-        {
+        }else if (this.cursors.right.isDown){
             this.player.body.velocity.x = 150;
         }else{
             this.player.body.velocity.x = 0;
         }
 
-        
-        if(this.cursors.up.isDown)
-        {
+        if(this.cursors.up.isDown){
             this.player.body.velocity.y = -150;
-        }
-        else if (this.cursors.down.isDown)
-        {
+        } else if (this.cursors.down.isDown){
             this.player.body.velocity.y = 150;
-        }else{
+            this.scaleToPlayer();
+        } else {
             this.player.body.velocity.y = 0;
         }
     }
